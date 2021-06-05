d8e2d36225519d7b0079102c8f99b1342fb3fc8a
PatelKaran0104 <85392409+PatelKaran0104@users.noreply.github.com>
2021-06-05 15:36:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        This was an amazing bootchamp taken up by Shaurya Sinha.We covered
+        everything from Scartch including Javacript React.js,HTML.
+      </p>
+    </div>
+  );
+}
+export default Note;
